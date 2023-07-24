77d6c92fb992f7334c6401f71c08873ca490a3ab
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-07-24 17:10:54 +0200

    Task 2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+* selection_sort - sorts an array in ascending order
+* @array: array to be sorted
+* @size: size of the array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int smallest, s_index;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest = array[i];
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (smallest > array[j])
+			{
+				smallest = array[j];
+				s_index = j;
+			}
+		}
+		array[s_index] = array[i];
+		array[i] = smallest;
+		print_array(array, size);
+	}
+}
